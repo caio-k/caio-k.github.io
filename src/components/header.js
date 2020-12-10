e58ebe46502f82f79f382d910af354ecf1328007
caio-k <koiti2000@hotmail.com>
2020-12-10 15:27:50 -0300

    refactor header session

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,56 @@
 import React from "react";
 import "../styles/header.css";
 import photo from "../assets/photo.jpeg";
-import house from "../assets/house.svg";
 
 function Header() {
   return (
     <div className="header">
-      <div className="header-content">
-        <div className="first-column-header">
-          <h1>Hello, My Name is <span>Caio</span></h1>
-          <h2>I like to solve problems, especially when I help other people with my solution. I'm always trying to learn
-            during the process, just as I'm always open to new challenges and ready to face changes.</h2>
-        </div>
-        <div className="second-column-header">
-          <img src={photo} alt={"Me"} width="300" height="300"/>
-          <div>
-            <img src={house} alt={"House"} width="30" height="30"/>
-            <p>São Paulo</p>
+      <div className="header-first-part">
+        <h1>Hello, my name is <span>Caio</span>!</h1>
+        <p>I like to solve problems, especially when I help other people with my solution. I'm always trying to learn
+          during the process, just as I'm always open to new challenges and ready to face changes.</p>
+      </div>
+      <div className="header-second-part">
+        <div className="header-info-box">
+          <div className="header-profile-title">
+            Profile
+          </div>
+          <div className="header-profile-content">
+            <div className="header-profile-img">
+              <img src={photo} alt={"Me"} width="200" height="200"/>
+            </div>
+            <div className="header-profile-info">
+              <div>
+                <span className="object-type">SWEngineer </span>
+                <span className="object-name">me </span>
+                <span>= </span>
+                <span className="reserved">new </span>
+                <span className="object-type">Caio</span>
+                <span>();</span>
+              </div>
+              <br/>
+              <div>
+                <span className="object-name">me</span>
+                <span>.</span>
+                <span className="attribute">age </span>
+                <span>= 20;</span>
+              </div>
+              <div>
+                <span className="object-name">me</span>
+                <span>.</span>
+                <span className="attribute">location </span>
+                <span>= "Sao Paulo";</span>
+              </div>
+              <br/>
+              <div>
+                <span className="object-name">me</span>
+                <span>.</span>
+                <span className="attribute">introduceMyself</span>
+                <span>();</span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="waves">
-        <div className="wave wave1"/>
-        <div className="wave wave2"/>
-        <div className="wave wave3"/>
-        <div className="wave wave4"/>
       </div>
     </div>
   )
