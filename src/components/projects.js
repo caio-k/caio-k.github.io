1c186a90ddcd38290a045052e2e0fa9a7721ea3c
caio-k <koiti2000@hotmail.com>
2021-02-06 18:18:46 -0300

    add monthly expenses project

--- src/components/projects.js
+++ src/components/projects.js
@@ -10,9 +10,11 @@ function Projects() {
 
       <div className="projects-box">
         <div className="project">
-          <h3>Sorting Visualizer</h3>
+          <div className="project-symbol"><span>1</span></div>
+          <h5>Sorting Visualizer</h5>
           <div className="project-description">
-            <p>This is a visualization tool for sorting algorithms. I used AngularJS in this project.</p>
+            <p>This is a visualization tool for sorting algorithms. I used AngularJS in this project and hosted the
+              application on GitHub Pages.</p>
           </div>
           <div className="project-links">
             <a href="https://github.com/caio-k/sorting-visualizer">Code</a>
@@ -21,10 +23,13 @@ function Projects() {
         </div>
 
         <div className="project">
-          <h3>SafeSchool</h3>
+          <div className="project-symbol"><span>2</span></div>
+          <h5>Safe School</h5>
           <div className="project-description">
-            <p>SafeSchool is a tool to help schools in Covid-19's epoch. It aims to control the flow of students within
-              a school to avoid crowds. I used ReactJS on the front-end and Spring Boot (Java) on the back-end.</p>
+            <p>Safe School is a tool to help schools in Covid-19's epoch. It aims to control the flow of students within
+              a school to avoid crowds. I used ReactJS on the front-end and Spring Boot (Java) on the back-end, and
+              hosted
+              the application on Heroku.</p>
           </div>
           <div className="project-links">
             <a href="https://github.com/caio-k/rp2-project-webapp">Frontend</a>
@@ -34,7 +39,22 @@ function Projects() {
         </div>
 
         <div className="project">
-          <h3>Other Projects</h3>
+          <div className="project-symbol"><span>3</span></div>
+          <h5>Monthly Expenses</h5>
+          <div className="project-description">
+            <p>Monthly Expenses is a tool for managing personal expenses. I used ReactJS on the front-end and Spring
+              Boot (Java) on the back-end, and hosted the application on Heroku.</p>
+          </div>
+          <div className="project-links">
+            <a href="https://github.com/caio-k/monthly-expenses-webapp">Frontend</a>
+            <a href="https://github.com/caio-k/monthly-expenses-server">Backend</a>
+            <a href="https://monthly-expenses-app.herokuapp.com/">Application</a>
+          </div>
+        </div>
+
+        <div className="project">
+          <div className="project-symbol"><span className="infinite-symbol">∞</span></div>
+          <h5>Other Projects</h5>
           <div className="project-description">
             <p>You can see my other projects on my GitHub.</p>
 
