16ebdda00fd36448e6fc79efee9f333d797ef22a
caio-k <koiti2000@hotmail.com>
2021-04-02 14:05:26 -0300

    refactor timeline

--- src/components/timelineItem.js
+++ src/components/timelineItem.js
@@ -3,18 +3,14 @@ import "../styles/timeline-item.css";
 
 const TimelineItem = ({data, index}) => (
   <div className={index % 2 === 0 ? "timeline-box left" : "timeline-box right"}>
-    <div className={index % 2 === 0 ? "timeline-content left-content" : "timeline-content right-content"}>
+    <div className={"timeline-content"}>
       <span>{data.institution} - {data.title}</span>
 
       <div>
         <span>From {data.begin} to {data.end}</span>
       </div>
 
-      <p>{data.description}</p>
-
-      {data.mainTechnologies && (
-        <p>{data.mainTechnologies}</p>
-      )}
+      {data.description.map((descriptionItem, index) => <p key={index}>{descriptionItem}</p>)}
     </div>
   </div>
 );
--- src/data/careerData.js
+++ src/data/careerData.js
@@ -2,15 +2,14 @@ const careerData = [
   {
     title: "Development Intern",
     institution: "Gradus Softwares de Gestão",
-    description: "Member of a team of six developers working on the construction and maintenance of frontend and backend of a budgeting SaaS, with the support of the quality team and business analysts, in order to translate the needs of customers into services",
-    mainTechnologies: "Java 8, MySQL, Javascript, AngularJS, HTML and CSS",
+    description: ["Member of a team of six developers working on the construction and maintenance of frontend and backend of a budgeting SaaS, with the support of the quality team and business analysts, in order to translate the needs of customers into services", "Main technologies: Java 8, MySQL, Javascript, AngularJS, HTML and CSS"],
     begin: "Jul 2019",
     end: "Feb 2020"
   },
   {
     title: "Information Systems Student",
     institution: "EACH-USP",
-    description: "Software Foundations; Computer Architecture; Algorithms; Complexity Analysis; Artificial Intelligence; Computational Theory; Operating Systems; Databases; Calculus II; Object Orientation; Data Structure.",
+    description: ["Software Foundations; Computer Architecture; Algorithms; Complexity Analysis; Artificial Intelligence; Computational Theory; Operating Systems; Databases; Calculus II; Object Orientation; Data Structure."],
     begin: "Mar 2018",
     end: "Dec 2021"
   }
